6d066efe2dfcbd3a80a7527329aa78b5c49fc7a8
Tanishaa08 <97739347+Tanishaa08@users.noreply.github.com>
2024-05-02 16:37:30 +0530

    Update app_constants.dart

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-   static const String appId = "APP_ID";
-   static const String authKey = "AUTH_KEY";
-   static const String region = "REGION";
+   static const String appId = "256832311436e43f";
+   static const String authKey = "945f3fb5850fde596053ceb3a693d73a97172dab";
+   static const String region = "in";
  }
